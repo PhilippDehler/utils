2bd860369b1e2e1e2d3be9c6e5232b1a3e6dc06b
PhilippDehler <philipp.dehler@esveo.com>
2024-11-17 17:30:39 +0100

    update

--- src/builder/VersionBuilder.ts
+++ src/builder/VersionBuilder.ts
@@ -47,3 +47,93 @@ function builder<T extends {}, V extends {}>(builts: T, versions: V) {
   };
   return self;
 }
+
+function deepFlat<T>(
+  arg: T,
+  currentPath: string = "",
+  flat: DeepFlat<T> = {} as DeepFlat<T>
+): DeepFlat<T> {
+  const _: any = flat;
+  _[currentPath] = arg;
+  if (arg === null) return flat;
+  if (typeof arg === "object")
+    Object.keys(arg).forEach((key: any) =>
+      (deepFlat as any)(
+        (arg as any)[key],
+        [currentPath, key].filter(Boolean).join("."),
+        _
+      )
+    );
+  return _;
+}
+
+const x = deepFlat({ a: { b: { c: 1 as const } } });
+const y = x["a.b.c"];
+
+type Entry = [string, unknown];
+
+type $deepFlat<
+  Arg,
+  Path extends string = "",
+  $agg extends Entry[] = [],
+  $next extends Entry[] = [...$agg, [Path, Arg]]
+> = Arg extends object
+  ? Arg extends any[]
+    ? IsTuple<Arg> extends false
+      ? DeepFlatArray<Arg, Path, $next>
+      : DeepFlatTuple<Arg, Path, $next>
+    : DeepFlatObject<Arg, Path, $next>
+  : $next;
+
+type DeepFlat<T> = $deepFlat<T> extends infer A extends Entry[]
+  ? FromEntries<A[number]>
+  : never;
+
+type DeepFlatTuple<
+  Arg extends any[],
+  BasePath extends string = "",
+  $agg extends Entry[] = [],
+  $count extends Num = []
+> = Arg extends [infer H, ...infer Tail]
+  ? $deepFlat<
+      H,
+      Concat<BasePath, $count["length"]>
+    > extends infer A extends Entry[]
+    ? DeepFlatTuple<Tail, BasePath, [...$agg, ...A], $inc<$count>>
+    : never
+  : $agg;
+
+type DeepFlatArray<
+  Arg extends any[],
+  BasePath extends string = "",
+  $agg extends Entry[] = []
+> = Arg extends (infer Item)[]
+  ? $deepFlat<Item, Concat<BasePath, number>, $agg>
+  : [];
+type DeepFlatObject<
+  Arg extends object,
+  BasePath extends string = "",
+  $agg extends Entry[] = []
+> = [
+  ...$agg,
+  ...{
+    [K in keyof Arg]: $deepFlat<Arg[K], Concat<BasePath, CastKey<K>>>;
+  }[keyof Arg]
+];
+
+type Concat<A extends string, B extends string | number> = A extends ""
+  ? `${B}`
+  : `${A}.${B}`;
+type CastKey<T> = T & (string | number);
+type FromEntries<T extends Entry> = {
+  [K in T[0]]: Extract<T, [K, unknown]>[1];
+};
+
+type Num = 0[];
+type $inc<T extends Num> = [...T, 0];
+
+type IsTuple<T extends any[]> = T["length"] extends number
+  ? number extends T["length"]
+    ? false
+    : true
+  : false;
--- src/enum.ts
+++ src/enum.ts
@@ -1,81 +1,158 @@
-namespace Enum {
-  export type Member = string | number;
-  export type Object<T extends readonly Member[]> = {
-    [K in T[number]]: K;
-  };
+type Member = string | number;
+type EnumObject<T extends readonly Member[]> = {
+  [K in T[number]]: K;
+};
 
-  type Guards<T extends readonly Member[]> = {
-    [K in T[number] as `is${Capitalize<`${K}`>}`]: (
-      value: unknown
-    ) => value is K;
-  } & { isMember: (value: unknown) => value is T[number] };
+type Guards<T extends readonly Member[]> = {
+  [K in T[number] as `is${Capitalize<`${K}`>}`]: (value: unknown) => value is K;
+} & {
+  isMember: (value: unknown) => value is T[number];
+  hasPropertyOf: <
+    TObject extends object,
+    Key extends keyof TObject,
+    Value extends TObject[Key] & T[number]
+  >(
+    obj: TObject,
+    prop: Key,
+    value: Value
+  ) => obj is Extract<TObject, { [K in Key]: Value }>;
+};
 
-  interface Utils<T extends readonly Member[]> {
-    match: <
-      Matcher extends { [K in T[number]]: () => unknown } & {
-        otherwise: (v: any) => unknown;
-      }
-    >(
-      matcher: Matcher
-    ) => (value: unknown) => ReturnType<Matcher[keyof Matcher]>;
-    with: <Strategy extends { [K in T[number]]: () => unknown }>(
-      args: Strategy
-    ) => <Val extends T[number]>(value: Val) => ReturnType<Strategy[Val]>;
-    mapValues: <const X extends { [K in keyof Enum.Object<T>]: unknown }>(
-      mappedEnum: X
-    ) => X;
-    values: () => T;
-    guards: Guards<T>;
-  }
-
-  export type Enum<T extends readonly Member[]> = Enum.Object<T> & Utils<T>;
-
-  const guardKey = <const T extends Member>(m: T): `is${Capitalize<`${T}`>}` =>
-    `is${m.toString()[0]!.toUpperCase()}${m.toString().slice(1)}` as any;
-  function initGuards<T extends readonly Member[]>(
-    memberSet: Set<T[number]>
-  ): Guards<T> {
-    const guards: any = {
-      isMember: (value: unknown): value is T[number] =>
-        memberSet.has(value as any),
-    } as any;
-    for (const member of memberSet.values()) {
-      const key = guardKey(member);
-      if (guards[key]) throw new Error("Enum init error.");
-      guards[key] = ((value: unknown) => member === value) as any;
+interface Utils<T extends readonly Member[]> {
+  /**
+   * Matches a value to a corresponding handler in the matcher object.
+   * If no match is found, the 'otherwise' handler is called.
+   *
+   * @param matcher - An object where each key is a member of T and has a corresponding handler function.
+   * @returns A function that takes a value and returns the result of the matched handler or 'otherwise'.
+   *
+   * @example
+   * const matcher = Enum.fromArray(["A", "B", "C"]).match({
+   *  A: () => "A",
+   * B: () => "B",
+   * otherwise: () => "C",
+   * });
+   * matcher("A"); // "A"
+   * matcher("B"); // "B"
+   * matcher("C"); // "C"
+   * matcher("D"); // "C"
+   *
+   * */
+  match: <
+    Matcher extends { [K in T[number]]?: () => unknown } & {
+      otherwise: (v: any) => unknown;
     }
-    return guards as Guards<T>;
-  }
+  >(
+    matcher: Matcher
+  ) => (
+    value: unknown
+  ) =>
+    | ReturnType<Matcher[keyof Matcher] & {}>
+    | ReturnType<Matcher["otherwise"]>;
+  /**
+   * Matches a value to a corresponding handler in the matcher object.
+   * If no match is found, an error is thrown.
+   *
+   * @param matcher - An object where each key is a member of T and has a corresponding handler function.
+   * @returns A function that takes a value and returns the result of the matched handler or throws an error.
+   * @example
+   * //NOTE: Typescript error due to missing handler for "C" this would be caught at compile time.
+   * const matcher = Enum.fromArray(["A", "B", "C"]).with({
+   * A: () => "A",
+   * B: () => "B",
+   * });
+   * matcher("A"); // "A"
+   * matcher("B"); // "B"
+   * matcher("C"); // Error: Expected enum member: [A, B, C]. Received: C
+   * matcher("D"); // Error: Expected enum member: [A, B, C]. Received: D
+   */
+  with: <Strategy extends { [K in T[number]]: (key: K) => unknown }>(
+    args: Strategy
+  ) => <Val extends T[number]>(value: Val) => ReturnType<Strategy[Val]>;
+  /**
+   * Enforces to input an object with all members of the enum as keys.
+   * Returns the same object.
+   *
+   * @example
+   * const enumObject = Enum.fromArray(["A", "B", "C"]);
+   * const mappedEnum = enumObject.mapValues({
+   * A: 1,
+   * B: 2,
+   * C: 3,
+   * });
+   * // mappedEnum is {A: 1, B: 2, C: 3}
+   *
+   */
+  mapValues: <const X extends { [K in keyof EnumObject<T>]: unknown }>(
+    mappedEnum: X
+  ) => X;
+  values: () => T;
 
-  export function fromArray<const T extends readonly Member[]>(
-    members: T
-  ): Enum<T> {
-    const set = new Set(members);
-    const membersAsEnumObject: Object<T> = Object.fromEntries(
-      [...set].map((m) => [m, m] as const)
-    ) as any;
-    const enumUtils: Utils<T> = {
-      match: (matcher: any) => (value: any) =>
-        matcher[value]?.() ?? matcher.otherwise(value),
-      with: (matcher: any) => (value: any) =>
-        matcher[value]?.() ??
-        (() => {
-          throw new Error(
-            `Expected enum member: [${members.join(", ")}]. Received:${value}`
-          );
-        })(),
-      values: () => members,
-      mapValues: <const X extends { [K in keyof Object<T>]: unknown }>(
-        mappedEnum: X
-      ) => mappedEnum,
-      guards: initGuards(set),
-    };
-    return Object.assign(membersAsEnumObject, enumUtils);
-  }
+  guards: Guards<T>;
+}
 
-  export type Infer<T extends Enum<any>> = T extends Enum<
-    infer U extends readonly Member[]
-  >
-    ? U[number]
-    : never;
+export type Enum<T extends readonly Member[]> = EnumObject<T> & Utils<T>;
+
+const guardKey = <const T extends Member>(m: T): `is${Capitalize<`${T}`>}` =>
+  `is${m.toString()[0]!.toUpperCase()}${m.toString().slice(1)}` as any;
+function initGuards<T extends readonly Member[]>(
+  memberSet: Set<T[number]>
+): Guards<T> {
+  const guards: any = {
+    isMember: (value: unknown): value is T[number] =>
+      memberSet.has(value as any),
+  } as any;
+  for (const member of memberSet.values()) {
+    const key = guardKey(member);
+    if (guards[key]) throw new Error("Enum init error.");
+    guards[key] = ((value: unknown) => member === value) as any;
+  }
+  guards["hasPropertyOf"] = hasPropertyOf;
+
+  return guards as Guards<T>;
+}
+
+export function fromArray<const T extends readonly Member[]>(
+  members: T
+): Enum<T> {
+  const set = new Set(members);
+  const membersAsEnumObject: EnumObject<T> = Object.fromEntries(
+    [...set].map((m) => [m, m] as const)
+  ) as any;
+  const enumUtils: Utils<T> = {
+    match: (matcher: any) => (value: any) =>
+      matcher[value]?.() ?? matcher.otherwise(value),
+    with: (matcher: any) => (value: any) =>
+      matcher[value]?.(value) ??
+      (() => {
+        throw new Error(
+          `Expected enum member: [${members.join(", ")}]. Received:${value}`
+        );
+      })(),
+    values: () => members,
+    mapValues: <const X extends { [K in keyof EnumObject<T>]: unknown }>(
+      mappedEnum: X
+    ) => mappedEnum,
+    guards: initGuards(set),
+  };
+  return Object.assign(membersAsEnumObject, enumUtils);
+}
+
+export type Infer<T extends Enum<any>> = T extends Enum<
+  infer U extends readonly Member[]
+>
+  ? U[number]
+  : never;
+
+function hasPropertyOf<T, Key extends keyof T, Value extends T[Key]>(
+  obj: T,
+  prop: Key,
+  value: Value
+): obj is Extract<T, { [K in Key]: Value }> {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    prop in obj &&
+    obj[prop] === value
+  );
 }
